86fa3d9aa1de138b71306b376a405659f693efb5
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-05 15:25:24 +0900

    Pro

--- AilenPrjTIMER01/ViewController.swift
+++ AilenPrjTIMER01/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 1
+    var myTimer = Timer()
+    var de = true
+    @IBOutlet weak var alienImageView: UIImageView!
+    
+    @IBOutlet weak var lbl01: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageView.image = UIImage(named: "frame\(counter).png")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.doAnimation), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @objc func doAnimation(){
+        if counter == 5{
+            de = false
+        } else if counter == 1{
+            de = true
+        }
+        if de==false{
+            counter = counter - 1
+        } else {
+            counter = counter + 1
+        }
+        
+        
+        alienImageView.image = UIImage(named: "frame\(counter).png")
+        lbl01.text = String(counter)
+    
 }
-
+}
